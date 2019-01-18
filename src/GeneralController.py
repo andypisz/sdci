ea09369396576edc208587e388166d20a6446138
andypisz <a.piszyna@gmail.com>
2019-01-18 09:58:19 +0100

    changing datacenter name

--- src/GeneralController.py
+++ src/GeneralController.py
@@ -12,13 +12,13 @@ class GeneralController:
         if self.requete == 1:
             self.IHM.printMessage("Entrez le nom de la VNF que vous souhaitez ajouter :")
             vnf_name = self.IHM.askUserInputVnfName()
-            url = 'http://127.0.0.1:5001/restapi/compute/cvim1/' + vnf_name
+            url = 'http://127.0.0.1:5001/restapi/compute/dc1/' + vnf_name
             data = '{"image":"ubuntu:trusty", "network":"(id=input,ip=10.0.0.1/24),(id=output,ip=20.0.0.1/24)"}'
             requests.put(url, data=data)
         elif self.requete == 2:
             self.IHM.printMessage("Entrez le nom de la VNF que vous souhaitez supprimer :")
             vnf_name = self.IHM.askUserInputVnfName()
-            url = 'http://127.0.0.1:5001/restapi/compute/cvim1/' + vnf_name
+            url = 'http://127.0.0.1:5001/restapi/compute/dc1/' + vnf_name
             requests.delete(url)
         elif self.requete == 3:
             print("case 3")
